5c0389d724fc861350429106e502f4adeec5daa3
Ken Perez <rken.perez@gmail.com>
2026-09-02 18:27:25 +0800

    chore: Added ratelimit on settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -80,6 +80,11 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
+
+        "DEFAULT_THROTTLE_RATES": {
+            "login": "10/min",
+        },
+        
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
