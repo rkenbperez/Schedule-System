25a54dfbd400f297728d34a8b97a50e55716e21b
Ken Perez <rken.perez@gmail.com>
2026-09-02 17:20:24 +0800

    feat: Added user models

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'drf_spectacular'
+    'drf_spectacular',
+    'users',
 ]
 
 MIDDLEWARE = [
