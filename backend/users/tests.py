8f99e118c0ac032557f4815c00cb57972cbc2b60
Ken Perez <rken.perez@gmail.com>
2026-09-02 18:36:31 +0800

    test: added throtle test user

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.cache import cache
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -9,8 +10,12 @@ from .models import Professors
 
 class LoginAPITests(APITestCase):
     def setUp(self):
+        cache.clear()
         self.user = User.objects.create_user(username="prof1", password="pass12345")
         Professors.objects.create(user=self.user, department="CCS")
+
+    def tearDown(self):
+        cache.clear()
 
     def test_login_returns_token(self):
         url = reverse("login")
@@ -32,3 +37,12 @@ class LoginAPITests(APITestCase):
             format="json",
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_throttled_after_limit(self):
+        url = reverse("login")
+        for _ in range(10):
+            self.client.post(url, {"username": "prof1", "password": "pass12345"}, format="json")
+        response = self.client.post(
+            url, {"username": "prof1", "password": "pass12345"}, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_429_TOO_MANY_REQUESTS)
